23f193b73834c5597664bd0ec84bf9f63266623d
treehouse-dave <dave@teamtreehouse.com>
2016-06-24 17:38:27 -0700

    Add form validation and Mongoose create statement

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 // GET /register
 router.get('/register', function(req, res, next) {
@@ -8,7 +9,41 @@ router.get('/register', function(req, res, next) {
 
 // POST /register
 router.post('/register', function(req, res, next) {
-  return res.send('User created!');
+  if (req.body.email &&
+    req.body.name &&
+    req.body.favoriteBook &&
+    req.body.password &&
+    req.body.confirmPassword) {
+
+      // confirm that user typed same password twice
+      if (req.body.password !== req.body.confirmPassword) {
+        var err = new Error('Passwords do not match.');
+        err.status = 400;
+        return next(err);
+      }
+
+      // create object with form input
+      var userData = {
+        email: req.body.email,
+        name: req.body.name,
+        favoriteBook: req.body.favoriteBook,
+        password: req.body.password
+      };
+
+      // use schema's `create` method to insert document into Mongo
+      User.create(userData, function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          return res.redirect('/profile');
+        }
+      });
+      
+    } else {
+      var err = new Error('All fields required.');
+      err.status = 400;
+      return next(err);
+    }
 })
 
 // GET /
