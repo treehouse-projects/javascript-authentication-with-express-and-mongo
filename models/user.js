4b8de5e41c95176fbd4832aab3d98f3703c1414b
treehouse-dave <dave@teamtreehouse.com>
2016-06-24 18:23:20 -0700

    Add Bcrypt and hash password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 var UserSchema = new mongoose.Schema({
     email: {
       type: String,
@@ -20,6 +21,17 @@ var UserSchema = new mongoose.Schema({
       type: String,
       required: true
     }
+});
+// hash password before saving to database
+UserSchema.pre('save', function(next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function(err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
 });
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
