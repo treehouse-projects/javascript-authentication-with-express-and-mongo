75e3c07d4d0dcfa19f86c8ef5fcf1578a527f6f8
treehouse-dave <dave@teamtreehouse.com>
2016-06-26 13:25:21 -0700

    Add profile route and template

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,23 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 
+// GET /profile
+router.get('/profile', function(req, res, next) {
+  if (! req.session.userId ) {
+    var err = new Error("You are not authorized to view this page.");
+    err.status = 403;
+    return next(err);
+  }
+  User.findById(req.session.userId)
+      .exec(function (error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          return res.render('profile', { title: 'Profile', name: user.name, favorite: user.favoriteBook });
+        }
+      });
+});
+
 // GET /login
 router.get('/login', function(req, res, next) {
   return res.render('login', { title: 'Log In'});
