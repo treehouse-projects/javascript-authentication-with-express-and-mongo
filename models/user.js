0a2c013764caee32b411106e616a16d14ddfbfd6
treehouse-dave <dave@teamtreehouse.com>
2016-06-26 12:49:59 -0700

    Add authentication method, add session

--- models/user.js
+++ models/user.js
@@ -22,6 +22,26 @@ var UserSchema = new mongoose.Schema({
       required: true
     }
 });
+// authenticate input against database documents
+UserSchema.statics.authenticate = function(email, password, callback) {
+  User.findOne({ email: email })
+      .exec(function (error, user) {
+        if (error) {
+          return callback(error);
+        } else if ( !user ) {
+          var err = new Error('User not found.');
+          err.status = 401;
+          return callback(err);
+        }
+        bcrypt.compare(password, user.password , function(error, result) {
+          if (result === true) {
+            return callback(null, user);
+          } else {
+            return callback();
+          }
+        })
+      });
+}
 // hash password before saving to database
 UserSchema.pre('save', function(next) {
   var user = this;
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,22 @@ router.get('/login', function(req, res, next) {
 
 // POST /login
 router.post('/login', function(req, res, next) {
-  return res.send('Logged In!');
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong email or password.');
+        err.status = 401;
+        return next(err);
+      }  else {
+        req.session.userId = user._id;
+        return res.redirect('/profile');
+      }
+    });
+  } else {
+    var err = new Error('Email and password are required.');
+    err.status = 401;
+    return next(err);
+  }
 });
 
 // GET /register
@@ -45,6 +60,7 @@ router.post('/register', function(req, res, next) {
         if (error) {
           return next(error);
         } else {
+          req.session.userId = user._id;
           return res.redirect('/profile');
         }
       });
