5aeb23b9aed4a82166e5df7b7429f11812a986b7
treehouse-dave <dave@teamtreehouse.com>
2016-06-26 09:52:32 -0700

    Add express-session

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var session = require('express-session');
 var app = express();
+
+// use sessions for tracking logins
+app.use(session({
+  secret: 'treehouse loves you',
+  resave: true,
+  saveUninitialized: false
+}));
 
 // mongodb connection
 mongoose.connect("mongodb://localhost:27017/bookworm");
