b5c2e5d9e07a20b4ced05655a9203bcab6f83600
treehouse-dave <dave@teamtreehouse.com>
2016-07-01 13:15:37 -0700

    Add password-protection middleware

--- middleware/index.js
+++ middleware/index.js
@@ -4,4 +4,14 @@ function loggedOut(req, res, next) {
   }
   return next();
 }
+function requiresLogin(req, res, next) {
+  if (req.session && req.session.userId) {
+    return next();
+  } else {
+    var err = new Error('You must be logged in to view this page.');
+    err.status = 401;
+    return next(err);
+  }
+}
 module.exports.loggedOut = loggedOut;
+module.exports.requiresLogin = requiresLogin;
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,7 @@ var User = require('../models/user');
 var mid = require('../middleware');
 
 // GET /profile
-router.get('/profile', function(req, res, next) {
-  if (! req.session.userId ) {
-    var err = new Error("You are not authorized to view this page.");
-    err.status = 403;
-    return next(err);
-  }
+router.get('/profile', mid.requiresLogin, function(req, res, next) {
   User.findById(req.session.userId)
       .exec(function (error, user) {
         if (error) {
